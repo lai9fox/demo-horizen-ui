74a1bddc2c79f1d2b253c39df14e11cb94e5b83e
赖玖湖 <jiuhulai@digitalgd.com.cn>
2022-12-21 16:43:58 +0800

    feat: 代码格式化配置更新

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,71 @@
-/* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution');
-
 module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier',
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
   rules: {
+    // 末尾分号
     semi: ['error', 'always'],
-    quotes: ['error', 'single'],
+    // 使用单引号
+    quotes: ['error', 'single', { avoidEscape: true }],
+    // 每个文件最后一行是空行
     'eol-last': ['error', 'always'],
+    // 不允许使用 var 关键字
+    'no-var': 'error',
+    'array-bracket-newline': ['error', { multiline: true }],
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: ['template', 'script', 'style']
+      }
+    ],
+    'vue/attributes-order': 'error', // 校验节点的属性顺序
+    'vue/order-in-components': [
+      'error',
+      {
+        order: [
+          'el',
+          'name',
+          'key',
+          'parent',
+          'functional',
+          ['delimiters', 'comments'],
+          'bemNS',
+          ['components', 'directives', 'filters'],
+          'extends',
+          'mixins',
+          ['provide', 'inject'],
+          'ROUTER_GUARDS',
+          'layout',
+          'middleware',
+          'validate',
+          'scrollToTop',
+          'transition',
+          'loading',
+          'inheritAttrs',
+          'model',
+          ['props', 'propsData'],
+          'emits',
+          'setup',
+          'asyncData',
+          'data',
+          'fetch',
+          'head',
+          'computed',
+          'watch',
+          'watchQuery',
+          'LIFECYCLE_HOOKS',
+          'methods',
+          ['template', 'render'],
+          'renderError'
+        ]
+      }
+    ]
   },
   parserOptions: {
-    ecmaVersion: 'latest',
-  },
+    ecmaVersion: 'latest'
+  }
 };
--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="item">
     <i>
+      123
       <slot name="icon"></slot>
     </i>
     <div class="details">
